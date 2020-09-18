d0dd6371a37ade1b3ce4c86b35c68d76623f279f
andrealoddo <andrea.loddo@unica.it>
2020-09-18 12:59:10 +0200

    Updated parseSubjectStatus for Matlab's version management

--- parseSubjectStatus.m
+++ parseSubjectStatus.m
@@ -1,11 +1,23 @@
 function CDRvalue = parseSubjectStatus(filepath)
 
-    text = readtable(filepath,'ReadVariableNames',false);    
-    
-    if( isempty( str2num( text{7,2}{1} ) ) )
-        CDRvalue = 0;
+    if verLessThan('matlab','9.8')
+        % -- Code to run in MATLAB earlier than R2020a --
+        text = readtable(filepath,'ReadVariableNames',false);
+
+        if( isempty( str2num( text{7,2}{1} ) ) )
+            CDRvalue = 0;
+        else
+            CDRvalue = str2double( text{7,2}{1} );
+        end
     else
-        CDRvalue = str2double( text{7,2}{1} );
+        % -- Code to run in MATLAB R2020a and later here --
+        text = readtable(filepath,'ReadVariableNames',false);
+
+        if( isnumeric( text{6,2} ) )
+            CDRvalue = text{6,2};
+        else
+            CDRvalue = 0;
+        end
     end
-    
+
 end
