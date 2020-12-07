51617dd3d13c0b8d01bc3cbb37086ad0b0a18b0a
sbuttau <sara.buttau@hotmail.it>
2020-12-07 11:34:58 +0100

    changes in Googlenet and Resnet50 script OASIS

--- OASIS DATASET/BINARIA/Googlenet/TrainingGooglenetBinary.m
+++ OASIS DATASET/BINARIA/Googlenet/TrainingGooglenetBinary.m
@@ -1,44 +1,98 @@
+% BINARY CLASSIFICATION - (healthy,dementia)
+% IMMAGINI LATERALI
+% STANDARD TRANSFER LEARNING - GOOGLENET
 
 %-----------------------  Gestione dataset --------------------------
-%Estrazione del dataset di immagini e delle labels corrispondenti
+%Inizializzazione variabili
 k = 1;
 n = 1;
 u = 1;
 v = 1;
-filepath1 = "Dataset\disc" + num2str(n);
-filepath2 = "\OAS1_000";
-filepath3 = "_MR1\OAS1_000";
-filepath4 = "_MR1.txt";
+q = 1;
 i = 1;
 
-for z = 1 : numel(dir(fullfile("Dataset\","disc*"))) %Scorre i dischi
-    for j = 1 :numel(dir(fullfile(filepath1,"OAS1_*"))) %Scorre le cartelle nei dischi
-    
-        if i > 9 
-            filepath2 = "\OAS1_00";
-            filepath3 = "_MR1\OAS1_00";
+healthyLabels =[];
+dementLabels = [];
+
+augSize = 224;
+classNumber = 2;
+
+%Controllo path locali - server
+if ispc
+
+  datasetpath = "Dataset\";
+  filepath1_baseline = "Dataset\disc";
+  filepath1 = "Dataset\disc" + num2str(n);
+  filepath2 = "\OAS1_000";
+  filepath3 = "_MR1\OAS1_000";
+  filepath4 = "_MR1.txt";
+
+  filepath2_bis = "\OAS1_00";
+  filepath3_bis = "_MR1\OAS1_00";
+
+  filepath2_tris = "\OAS1_0";
+  filepath3_tris = "_MR1\OAS1_0";
+
+  folder = "_MR1\RAW";
+  
+  
+  models_folder = "Binary/Training/Googlenet";
+  model_name = "googlenet_binary_oasis_300ep_128mbs";
+
+else
+
+  datasetpath = "/home/server/MATLAB/dataset/OASIS/";
+  filepath1_baseline = "/home/server/MATLAB/dataset/OASIS/disc";
+  filepath1 = "/home/server/MATLAB/dataset/OASIS/disc" + num2str(n);
+  filepath2 = "/OAS1_000";
+  filepath3 = "_MR1/OAS1_000";
+  filepath4 = "_MR1.txt";
+
+  filepath2_bis = "/OAS1_00";
+  filepath3_bis = "_MR1/OAS1_00";
+
+  filepath2_tris = "/OAS1_0";
+  filepath3_tris = "_MR1/OAS1_0";
+
+  folder = "_MR1/RAW";
+
+  models_folder = "models/";
+  model_name = "googlenet_binary_oasis_300ep_128mbs";
+end
+
+
+%Estrazione del dataset di immagini e delle labels corrispondenti
+
+for z = 1 : numel(dir(fullfile(datasetpath, "disc*"))) %Scorre i dischi
+    for j = 1 :numel(dir(fullfile(filepath1, "OAS1_*"))) %Scorre le cartelle nei dischi
+
+        if i > 9
+            filepath2 = filepath2_bis;
+            filepath3 = filepath3_bis;
         end
         if i > 99
-            filepath2 = "\OAS1_0";
-            filepath3 = "_MR1\OAS1_0";
+            filepath2 = filepath2_tris;
+            filepath3 = filepath3_tris;
         end
+
         %Genero il path del file di testo
         filepath = filepath1 + filepath2 + num2str(i)+ filepath3 + num2str(i) + filepath4;
-        R = filepath1 + filepath2 + num2str(i) + "_MR1\RAW"; %Path della cartella RAW
+        R = filepath1 + filepath2 + num2str(i) + folder; %Path della cartella
         S = dir(fullfile(R,'*.gif'));
         array = strings(1,numel(S)); %Inizializzo l'array
         CDR = parseSubjectStatus(filepath); %Ricavo il Clinical Dementia Rating (livello di demenza)
-          if CDR == 0 %Se è nullo, il paziente è sano
-              healthyIndx(u) = CDR; %Salvo l'indice
+        if CDR == 0 %Se è nullo, il paziente è sano
+            healthyIndx(u) = CDR; %Salvo l'indice
               %Salvo le immagini in un cell array   
               for k = 1: numel(S)
                  array(k) = fullfile(R,S(k).name);
               end
               healthyImgs{u} = array;
               u=u+1;
-          end
-        if CDR > 0 %Se è > 0, il paziente è affetto da demenza
-            
+
+        end
+
+        if CDR > 0 %Se >0, il paziente è affetto da demenza
             dementIndx(v) = CDR; % Salvo l'indice
             %Salvo le immagini in un cell array 
             for k = 1: numel(S)
@@ -46,11 +100,11 @@ for z = 1 : numel(dir(fullfile("Dataset\","disc*"))) %Scorre i dischi
             end
             dementImgs{v} = array;
             v = v+1;
-        end     
-    i = i+1;
-    end   
+        end
+ i = i+1;
+    end
     n = n+1;
-    filepath1 = "Dataset\disc" + num2str(n);
+    filepath1 = filepath1_baseline + num2str(n);
 end
 
 % Conversione del cell array dementImgs ad array di stringhe per la
@@ -84,23 +138,22 @@ dementDs.Labels = dementLabels; %Etichetta della classe "dementia"
 ds = imageDatastore(cat(1,healthyDs.Files,dementDs.Files));
 ds.Labels = cat(1,healthyDs.Labels,dementDs.Labels);
 ds = shuffle(ds);
-%ds = combine(healthyDs,dementDs); %Datastore combinato
 
 % Divisione tra training set(80%),validation set (10%) e test set (10%)
 [trainImgs,valSet,testImgs] = splitEachLabel(ds,0.8,0.1,0.1,'randomized');
-testImgs.ReadFcn = @(filename)gray2rgb_resize(filename,224); %Vengono ridimensionate le immagini del test
+testImgs.ReadFcn = @(filename)gray2rgb_resize(filename,augSize); %Vengono ridimensionate le immagini del test
 % Viene applicata l'augmentation al training set per aumentare la diversità
 % all'interno del dataset
 imageAugmenter = imageDataAugmenter("RandRotation",[-35 35],"RandXScale",[0.5 4],"RandYScale",[0.5 1]);
-trainAug = augmentedImageDatastore([224 224],trainImgs,"ColorPreprocessing","gray2rgb","DataAugmentation",imageAugmenter);
-valAug = augmentedImageDatastore([224 224],valSet,"ColorPreprocessing","gray2rgb","DataAugmentation",imageAugmenter);
+trainAug = augmentedImageDatastore([augSize augSize],trainImgs,"ColorPreprocessing","gray2rgb","DataAugmentation",imageAugmenter);
+valAug = augmentedImageDatastore([augSize augSize],valSet,"ColorPreprocessing","gray2rgb","DataAugmentation",imageAugmenter);
 
 % ------------------   Creazione della rete   --------------------------
 net = googlenet;
 layers = layerGraph(net);
 [layerToReplace,classLayer] = findLayersToReplace(layers);
 % Modifico il terzultimo e l'ultimo strato della rete
-newFCLayer = fullyConnectedLayer(2,'Name','new_fc'); % Classificazione binaria
+newFCLayer = fullyConnectedLayer(classNumber,'Name','new_fc'); % Classificazione binaria
 layers = replaceLayer(layers,layerToReplace.Name,newFCLayer);
 newCLLayer = classificationLayer('Name','new_output');
 layers = replaceLayer(layers,classLayer.Name,newCLLayer);
@@ -109,6 +162,7 @@ options = trainingOptions('adam',"Plots","training-progress","ValidationData",va
 
 % -------------------    Train Network    -------------------------------
 trainedNet = trainNetwork(trainAug,layers,options);
+save(fullfile(models_folder, model_name), "trainedNet");
 preds = classify(trainedNet, testImgs);
 accuracy = nnz(preds == testImgs.Labels)/numel(preds)
 
